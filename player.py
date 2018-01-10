2d23a9f9d8317704a5fc61dc96388de638b43088
Srivoknovskiy-home <sri@gmail.com>
2018-01-10 22:24:39 +0200

    init

--- player.py
+++ player.py
@@ -20,5 +20,14 @@ class GTAPlayer:
     def release(self, key):
         ReleaseKey(key)
 
+    def releaseForward(self):
+        ReleaseKey(W)
+
+    def releaseLeft(self):
+        ReleaseKey(A)
+
+    def releaseRight(self):
+        ReleaseKey(D)
+
     def getPressedKeys(self):
         return []
